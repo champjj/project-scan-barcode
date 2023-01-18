789b1d8e6c2c10f94e8ac70a06d4832553ba9089
champjj <champ.j@outlook.com>
2023-01-18 18:01:25 +0700

    remove useless function

--- src/app/pages/sell-product/sell-product.component.ts
+++ src/app/pages/sell-product/sell-product.component.ts
@@ -2,7 +2,6 @@ import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import {
   ScannerQRCodeConfig,
   ScannerQRCodeSelectedFiles,
-  NgxScannerQrcodeService,
   ScannerQRCodeResult,
 } from 'ngx-scanner-qrcode';
 @Component({
@@ -11,8 +10,9 @@ import {
   styleUrls: ['./sell-product.component.scss'],
 })
 export class SellProductComponent {
+  value = '';
   // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-  public config: ScannerQRCodeConfig = {
+  config: ScannerQRCodeConfig = {
     // fps: 1000,
     // isAuto: false,
     // isBeep: true,
@@ -25,38 +25,20 @@ export class SellProductComponent {
     },
   };
 
-  public qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
-  public qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
+  qrCodeResult: ScannerQRCodeSelectedFiles[] = [];
+  qrCodeResult2: ScannerQRCodeSelectedFiles[] = [];
 
-  constructor(private qrcode: NgxScannerQrcodeService) {}
+  constructor() {}
 
-  public onEvent(e: ScannerQRCodeResult[]): void {
-    alert(e[0].value);
+  ngOnInit() {}
+
+  onEvent(e: ScannerQRCodeResult[]): void {
+    this.value = e[0].value;
+    alert('data' + e[0].value);
     console.log(e);
   }
 
-  public handle(action: any, fn: string): void {
+  handle(action: any, fn: string): void {
     action[fn]().subscribe(console.log, alert);
-  }
-
-  public onDowload(action: any) {
-    action.download().subscribe(console.log, alert);
-  }
-
-  public onSelects(files: any) {
-    this.qrcode
-      .loadFiles(files)
-      .subscribe((res: ScannerQRCodeSelectedFiles[]) => {
-        this.qrCodeResult = res;
-      });
-  }
-
-  public onSelects2(files: any) {
-    this.qrcode
-      .loadFilesToScan(files, this.config)
-      .subscribe((res: ScannerQRCodeSelectedFiles[]) => {
-        console.log(res);
-        this.qrCodeResult2 = res;
-      });
   }
 }
