5c1805769a41b47c35951b6de9d525c029aa8c46
champjj <champ.j@outlook.com>
2023-01-19 12:51:05 +0700

    show data in real data#2

--- src/app/pages/sell-product/sell-product.component.ts
+++ src/app/pages/sell-product/sell-product.component.ts
@@ -31,6 +31,33 @@ export class SellProductComponent implements OnInit {
     },
   ];
 
+  data = {
+    closed: false,
+    currentObservers: [],
+    observers: [],
+    isStopped: false,
+    hasError: false,
+    thrownError: null,
+    _value: [
+      {
+        deviceId:
+          '9764f941a3501fb758bcf681a998d4f2dfd29a5532ddc39e41285b7453199add',
+        kind: 'videoinput',
+        label: 'camera2 1, facing front',
+        groupId:
+          '6476d62b3d75a282ca110c191062be7d07287f0ce620aecfd62a29807127bf16',
+      },
+      {
+        deviceId:
+          '0fe69b7ae2d3350b9ffad5e32a6d4789b50796403772ec0c9f6d1c6555c56841',
+        kind: 'videoinput',
+        label: 'camera2 0, facing back',
+        groupId:
+          'ab1e2dc5cf22dced4917f8cd4d019c8e9279b93afb3bea8c6c835fa06f1eba7d',
+      },
+    ],
+  };
+
   showData: any;
 
   deviceIdFacingback: any;
@@ -64,14 +91,13 @@ export class SellProductComponent implements OnInit {
     action[fn]()
       .pipe(
         tap(() => {
-          // const findFacingBack = action['devices'].findIndex(
-          //   (data: any) => data.label == 'camera2 0, facing back'
-          // );
-          this.showData = action.devices;
-          // this.indexCameraFacingback = findFacingBack;
-          // action.devices.find(data);
-          // this.deviceIdFacingback = action.devices[findFacingBack].deviceId;
-          // action.playDevice(action.devices[findFacingBack].deviceId);
+          const findFacingBack = action.devices._value.findIndex(
+            (data: any) => data.label == 'camera2 0, facing back'
+          );
+          this.showData = action.devices._value;
+          this.indexCameraFacingback = findFacingBack;
+          this.deviceIdFacingback = action.devices[findFacingBack].deviceId;
+          action.playDevice(action.devices[findFacingBack].deviceId);
         })
       )
       .subscribe(console.log, alert);
