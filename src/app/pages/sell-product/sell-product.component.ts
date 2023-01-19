674ff53a98eca325d6292fea40224b3adb0fb5da
champjj <champ.j@outlook.com>
2023-01-19 13:40:30 +0700

    test scanner result

--- src/app/pages/sell-product/sell-product.component.ts
+++ src/app/pages/sell-product/sell-product.component.ts
@@ -13,56 +13,12 @@ import { IScannerDevice } from 'src/app/@core/models/scanner-models';
   styleUrls: ['./sell-product.component.scss'],
 })
 export class SellProductComponent implements OnInit {
-  testDevices = [
-    {
-      deviceId:
-        '9764f941a3501fb758bcf681a998d4f2dfd29a5532ddc39e41285b7453199add',
-      kind: 'videoinput',
-      label: 'camera2 1, facing front',
-      groupId:
-        '534dd109ed61981065d28507586f490aa13c9d5661c8bc3404ae32f59bed45b9',
-    },
-    {
-      deviceId:
-        '0fe69b7ae2d3350b9ffad5e32a6d4789b50796403772ec0c9f6d1c6555c56841',
-      kind: 'videoinput',
-      label: 'camera2 0, facing back',
-      groupId:
-        '41d152dac4bf35acb5ed3b26dece6cfdfe1d4582e81d8075098f823693ca6bbb',
-    },
-  ];
-
-  data = {
-    closed: false,
-    currentObservers: [],
-    observers: [],
-    isStopped: false,
-    hasError: false,
-    thrownError: null,
-    _value: [
-      {
-        deviceId:
-          '9764f941a3501fb758bcf681a998d4f2dfd29a5532ddc39e41285b7453199add',
-        kind: 'videoinput',
-        label: 'camera2 1, facing front',
-        groupId:
-          '6476d62b3d75a282ca110c191062be7d07287f0ce620aecfd62a29807127bf16',
-      },
-      {
-        deviceId:
-          '0fe69b7ae2d3350b9ffad5e32a6d4789b50796403772ec0c9f6d1c6555c56841',
-        kind: 'videoinput',
-        label: 'camera2 0, facing back',
-        groupId:
-          'ab1e2dc5cf22dced4917f8cd4d019c8e9279b93afb3bea8c6c835fa06f1eba7d',
-      },
-    ],
-  };
-
   showData: any;
 
   deviceIdFacingback: any;
   indexCameraFacingback: any;
+
+  showScannerResult:any
 
   value = '';
   // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
@@ -91,8 +47,9 @@ export class SellProductComponent implements OnInit {
   handle(action: any, fn: string): void {
     action[fn]()
       .pipe(
-        tap(() => {
+        tap((value) => {
           this.showData = action.devices._value;
+          this.showScannerResult = value
         })
       )
       .subscribe(() => {
@@ -102,13 +59,6 @@ export class SellProductComponent implements OnInit {
   }
 
   setCameraFacingback(action: any, data: IScannerDevice[]): void {
-    // const findFacingBack = action!.devices!._value.findIndex(
-    //   (data: any) => data.label == 'camera2 0, facing back'
-    // );
-    // this.indexCameraFacingback = findFacingBack;
-    // this.deviceIdFacingback = action.devices[findFacingBack]!.deviceId;
-    // action.playDevice(action.devices[findFacingBack]!.deviceId);
-
     const findFacingback = data.findIndex(
       (data: IScannerDevice) => data.label == 'camera2 0, facing back'
     );
