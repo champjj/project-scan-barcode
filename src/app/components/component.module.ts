a5c377ac8113647d1d3fe727c2e6af235778b3b1
champjj <champ.j@outlook.com>
2023-02-13 17:56:37 +0700

    downgrade material v14

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -5,6 +5,7 @@ import { CInputComponent } from './c-input/c-input.component';
 
 ///// material
 import { MatInputModule } from '@angular/material/input';
+
 import { CScanBarcodeComponent } from './c-scan-barcode/c-scan-barcode.component';
 import { DirectiveModule } from '../@core/directives/directive.module';
 
